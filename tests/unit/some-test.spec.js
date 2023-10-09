a8835cac844e1fa8bfc068b3b88d886177e311ca
razbroc <razbroc@gmail.com>
2023-10-09 13:33:27 +0300

    chore: upload for raz in panda

--- tests/unit/some-test.spec.js
+++ tests/unit/some-test.spec.js
@@ -1,16 +1,22 @@
 import { getWMTSCapabilities } from '../../src/capabilitiesManager.js';
-import { mockBaseUrl } from './mock/capabilitiesParams.js';
+import { mockBaseUrl, mockKvpUrl, mockRestUrl, mockParsedCapabilities } from './mock/capabilitiesParams.js';
 
 describe('getWMTSCapabilities', () => {
   it('should request from server and successfully return the requested layer', async () => {
     // action
     const resource = await getWMTSCapabilities(mockBaseUrl);
     const mockFetch = jest.fn();
+    const mockParseFromString = jest.fn();
     jest.mock('../../src/capabilitiesManager', () => ({
       fetch: mockFetch,
     }));
-    mockFetch.mockReturnValue('return value');
-    expect(1 + 1).toEqual(2);
+    jest.mock('../../src/capabilitiesManager', () => ({
+      parseFromString: mockParseFromString,
+    }));
+    mockFetch.mockReturnValue(mockRestUrl);
+    mockParseFromString.mockReturnValue(mockParsedCapabilities);
+
+    expect(resource.toEqual(new Capabilities(mockBaseUrl, domToJson(mockParsedCapabilities.documentElement))));
 
     // expectation;
   });
